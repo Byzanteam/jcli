772525db85ab4b1435d73dab2de9bab36166c152
CptBreeza <cbasicok@gmail.com>
2024-03-29 14:20:36 +0800

    fixup! Build subcommand to print logs

--- src/subcommands/function/logs/mod.ts
+++ src/subcommands/function/logs/mod.ts
@@ -5,7 +5,7 @@ import errorHandler from "@/error-handler.ts";
 import action from "./action.ts";
 
 const command = new Command<LogsOptions>()
-  .description("Print deployment logs")
+  .description("Print function logs")
   .arguments("[functionName:string]")
   .option("-n, --length <length:number>", "Length of logs to print")
   .option("--prod", "Print production logs")
