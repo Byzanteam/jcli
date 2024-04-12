23d751b341f7e27e583035578cf39f740a437cd5
Dane Wang <dwalters.maruko@gmail.com>
2024-04-12 15:52:37 +0800

    refactor: deploy action using jcli link

--- src/subcommands/generate/github-workflow/action.ts
+++ src/subcommands/generate/github-workflow/action.ts
@@ -26,9 +26,7 @@ jobs:
       - name: Deploy
         run: |
           cd "<%= project.distDir %>"
-          jcli clone "<%= project.id %>" .jcli-deploy-tmp
-          mv .jcli-deploy-tmp/.jcli ./
-          cp .jcli-deploy-tmp/project.json ./
+          jcli link "<%= project.id %>"
           jcli push
           jcli commit
           jcli deploy`,
