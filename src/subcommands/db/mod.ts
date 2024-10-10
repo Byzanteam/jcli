75a9f45df265b1f3ad9bc739fe8564a09b514195
Dane Walters <dwalters.maruko@gmail.com>
2024-10-10 16:01:22 +0800

    feat: generate migration file command

--- src/subcommands/db/mod.ts
+++ src/subcommands/db/mod.ts
@@ -4,11 +4,13 @@ import { GlobalOptions } from "@/args.ts";
 import migrateCommand from "@/subcommands/db/migrate/mod.ts";
 import rollbackCommand from "@/subcommands/db/rollback/mod.ts";
 import migrationsCommand from "@/subcommands/db/migrations/mod.ts";
+import generateCommand from "@/subcommands/db/migrations_generate/mod.ts";
 
 const command = new Command<GlobalOptions>()
   .description("Commands to manage your database on Jet.")
   .command("migrate", migrateCommand)
   .command("rollback", rollbackCommand)
-  .command("migrations", migrationsCommand);
+  .command("migrations", migrationsCommand)
+  .command("migrations:gen", generateCommand);
 
 export default command;
