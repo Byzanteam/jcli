06a09b38f6262a3ff9d8fcb244c71a7d70ccef2b
vanppo <vanppo@hotmail.com>
2024-12-20 16:54:17 +0800

    feature: push workflows

--- src/subcommands/push/mod.ts
+++ src/subcommands/push/mod.ts
@@ -8,6 +8,7 @@ const command = new Command<PushOptions>()
   .description("Push local changes to Jet.")
   .option("--only-functions", "Pushes only changes of functions")
   .option("--only-migrations", "Pushes only changes of migrations")
+  .option("--only-workflows", "Pushes only changes of workflows")
   .error(errorHandler)
   .action(action);
 
