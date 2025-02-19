701e92559f6afd0676d54e216c38d3800ce63ce3
Phil Chen <06fahchen@gmail.com>
2025-02-19 15:02:40 +0800

    fix: include type

--- src/subcommands/push/mod.ts
+++ src/subcommands/push/mod.ts
@@ -10,8 +10,8 @@ const command = new Command<PushOptions>()
   .description("Push local changes to Jet.")
   .type("include", includeType)
   .option(
-    "--include <category:category>",
-    "Pushes only changes of the specified category",
+    "--include <category:include>",
+    "Pushes only changes of the specified category.",
     { collect: true },
   )
   .error(errorHandler)
